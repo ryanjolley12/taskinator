42191209e0249fcc62751820408aadd3827fec7f
ryanjolley12 <ryanswims@gmail.com>
2021-06-07 12:09:19 -0400

    fix

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -12,6 +12,9 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function(event) { 
   event.preventDefault(); 
 
+  var taskNameInput = document.querySelector("input[name='task-name']");
+  console.log(taskNameInput);
+
   var listItemEl = document.createElement("li"); 
   listItemEl.className = "task-item"; 
   listItemEl.textContent = "This is a new task."; 
