61707699fa74eb285d28713451f73797900bc228
ryanjolley12 <ryanswims@gmail.com>
2021-06-08 11:29:45 -0400

    first commit

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,3 +1,5 @@
+var taskIdCounter = 0;
+
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
@@ -31,6 +33,11 @@ var createTaskEl = function (taskDataObj) {
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
 
+  //going to use the taskIdCounter to assign an id to the current task being created using data-* attribute (data-task-id)
+  //add task id as a custom attribute 
+  listItemEl.setAttribute("data-task-id", taskIdCounter);
+
+
   // create div to hold task info and add to list item
   var taskInfoEl = document.createElement("div");
   taskInfoEl.className = "task-info";
@@ -39,6 +46,10 @@ var createTaskEl = function (taskDataObj) {
 
   // add entire list item to list
   tasksToDoEl.appendChild(listItemEl);
+
+  //increase task counter for next unique id
+  taskIdCounter++;
+
 };
 
 formEl.addEventListener("submit", taskFormHandler);
